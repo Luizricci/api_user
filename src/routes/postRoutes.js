f82be3490ed5a8553a100d0f25d57f745994c3da
Luiz_ricci <luizaureliano.tds1.senai@gmail.com>
2025-04-03 13:44:26 -0300

    Feat: finish post routes

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -5,5 +5,9 @@ const router = express.Router();
 const postController = require("../controllers/postControllers");
 
 router.get("/", postController.getAllPosts);
+router.get("/:id", postController.getById);
+router.post("/", postController.createPost);
+router.put("/:id", postController.editPost);
+router.delete("/:id", postController.deletePost);
 
 module.exports = router;
